30220c04a78d2397611e004860ca5ad8c4870bba
keegantm <moseley.k@northeastern.edu>
2025-01-06 16:37:58 -0500

    Progress, Got gui with EQ UI working

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -52,7 +52,13 @@ public:
     //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
-
+    
+    //this contains a Tree that is used to manage an AudioProcessor's entire state
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
+        
+    
+    
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
